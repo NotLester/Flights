9522c42eb1ebb9a49f266f678c5605417db55e0f
NotLester <lester.lewisudupi@gmail.com>
2023-07-21 09:55:42 +0530

    added get all airplane api

--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -11,13 +11,10 @@ async function createAirplane(data) {
 	} catch (error) {
 		if (error.name == 'SequelizeValidationError') {
 			let explanation = [];
-			error.errors.forEach(err => {
+			error.errors.forEach((err) => {
 				explanation.push(err.message);
 			});
-			throw new AppError(
-				explanation,
-				StatusCodes.BAD_REQUEST
-			);
+			throw new AppError(explanation, StatusCodes.BAD_REQUEST);
 		}
 		throw new AppError(
 			'Cannot create a new airplane object',
@@ -26,6 +23,19 @@ async function createAirplane(data) {
 	}
 }
 
+async function getAirplanes() {
+	try {
+		const airplanes = airplaneRepository.getAll();
+		return airplanes;
+	} catch (error) {
+		throw new AppError(
+			'Cannot fetch data of all airplanes',
+			StatusCodes.INTERNAL_SERVER_ERROR
+		);
+	}
+}
+
 module.exports = {
 	createAirplane,
+	getAirplanes,
 };
