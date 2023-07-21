13ccae73088ad4241233669b40a7242c98e4d3a3
NotLester <lester.lewisudupi@gmail.com>
2023-07-21 11:06:07 +0530

    added create city api

---
+++ src/routes/v1/city-routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { CityController } = require('../../controllers');
+const { AirplaneMiddlewares } = require('../../middlewares');
+
+// /api/v1/airplanes POST
+router.post('/', CityController.createCity);
+
+module.exports = router;
